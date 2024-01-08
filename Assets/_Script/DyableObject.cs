89c0865b222e51b78f199af6fe0186a9ff119282
Kaha Chen <>
2024-01-08 12:31:44 +0800

    add delay time

--- Assets/_Script/DyableObject.cs
+++ Assets/_Script/DyableObject.cs
@@ -12,7 +12,9 @@ namespace DigiHero
         [SerializeField] private Animator animator;
         [SerializeField] private string diedStateName;
         [SerializeField] private AudioClip diedClip;
+        [SerializeField] private float delayDiedEventTime = 1f;
         [SerializeField] private UnityEvent onDied;
+        [SerializeField] private float delayReviveEventTime = 1f;
         [SerializeField] private UnityEvent onRevived;
 
         private int diedStateHash;
@@ -24,6 +26,33 @@ namespace DigiHero
 
         private readonly object motionApplierLocker = new object();
         private bool isDied = false;
+
+        private float delayDiedEventTimer = 0f;
+        private float delayReviveEventTimer = 0f;
+
+        private void Update()
+        {
+            if (delayDiedEventTimer > 0f)
+            {
+
+                delayDiedEventTimer -= Time.deltaTime;
+                if (delayDiedEventTimer <= 0f)
+                {
+                    delayDiedEventTimer = 0f;
+                    onDied.Invoke();
+                }
+            }
+
+            if (delayReviveEventTimer > 0f)
+            {
+                delayReviveEventTimer -= Time.deltaTime;
+                if (delayReviveEventTimer <= 0f)
+                {
+                    delayReviveEventTimer = 0f;
+                    onRevived.Invoke();
+                }
+            }
+        }
 
         public void OnHpChanged(int cur)
         {
@@ -43,12 +72,12 @@ namespace DigiHero
 
                 isDied = true;
 
-                onDied.Invoke();
+                delayDiedEventTimer = delayDiedEventTime;
             }
             else if (cur > 0 && isDied)
             {
                 motionApplier.Resume(motionApplierLocker);
-                onRevived.Invoke();
+                delayReviveEventTimer = delayReviveEventTime;
             }
         }
     }
